c837107ac2cae9d78047d2e1cf1daf0ec269e40f
woogie-real <woogiereal@gmail.com>
2022-09-23 23:52:20 +0900

    add findDataById method

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ class HierarchyData {
   getParentIdKeyName() {
     return this.parentIdKeyName;
   }
+
+  findDataById = (datas, key) => {
+    let find = false;
+    for (let i = 0; i < datas.length; i ++) {
+      if (datas[i][this.idKeyName] === key) {
+        find = true;
+        return datas[i];
+      }
+    }
+  
+    if (!find) {
+      for (let data of datas) {
+        if (data.children) {
+          const result = this.findDataById(data.children, key);
+          if (result) {
+            return result;
+          }
+        }
+      }
+    }
+  
+    return null;
+  }
 }
 
 module.exports = HierarchyData;
--- index.test.js
+++ index.test.js
@@ -1,23 +1,40 @@
-const HierarchyData = require('./index.js');
-const mockData = require('./mockData.js');
+const HierarchyData = require("./index.js");
+const mockData = require("./mockData.js");
 
-const idKeyName = 'id';
-const parentIdKeyName = 'parentId';
+const idKeyName = "id";
+const parentIdKeyName = "parentId";
 
 const hierarchyData = new HierarchyData(idKeyName, parentIdKeyName);
 
-describe('getKey()', () => {
-  describe('success', () => {
-    test('return idKeyName', () => {
-      expect(hierarchyData.getIdKeyName()).toBe(idKeyName);
+describe("HierarchyData", () => {
+  describe("getKey()", () => {
+    describe("success", () => {
+      test("return idKeyName", () => {
+        expect(hierarchyData.getIdKeyName()).toBe(idKeyName);
+      });
     });
-  })
-})
+  });
 
-describe('getParentKey()', () => {
-  describe('success', () => {
-    test('return parentIdKeyName', () => {
-      expect(hierarchyData.getParentIdKeyName()).toBe(parentIdKeyName);
+  describe("getParentKey()", () => {
+    describe("success", () => {
+      test("return parentIdKeyName", () => {
+        expect(hierarchyData.getParentIdKeyName()).toBe(parentIdKeyName);
+      });
     });
-  })
-})
+  });
+
+  describe("findDataById()", () => {
+    describe("success", () => {
+      test("return data by id", () => {
+        const targetData = {
+          id: 1131,
+          type: 20,
+          name: "CASE",
+          parent: 1130,
+          children: null,
+        };
+        expect(hierarchyData.findDataById(mockData, 1131)).toEqual(targetData);
+      });
+    });
+  });
+});
