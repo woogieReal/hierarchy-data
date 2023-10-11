ea05ddfba2cfb05d911e88978feb4fe2a29e80ae
woogieReal <woogiereal@gmail.com>
2023-10-11 17:21:27 +0900

    feat: typescript 패키지로 전환

---
+++ lib/index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "@jest/globals";
+import HierarchyData from ".";
+
+const hierarchyData = new HierarchyData();
+
+describe("HierarchyData", () => {
+  describe("test", () => {
+    test("return true", () => {
+      expect(hierarchyData.test()).toEqual(true);
+    });
+  })
+})
---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export default class HierarchyData {
+  constructor() {}
+
+  test = () => {
+    return true;
+  }
+}
