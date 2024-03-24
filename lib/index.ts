d6794f1693e00167e49c881fdaa29be8c8446046
woogieReal <woogiereal@gmail.com>
2024-03-24 19:02:18 +0900

    feat: add function getConfig

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,15 @@
-const findup = require('findup-sync');
+import findup from 'findup-sync';
+import fs from 'fs';
+
+export const getConfig = () => {
+  const configFileFullPath = findup('hierarchy-data.json');
+
+  if (!configFileFullPath) {
+    throw new Error('Configuration file does not exist.');
+  }
+  
+  const jsonContents = fs.readFileSync(configFileFullPath).toString();
+  const confjg = JSON.parse(jsonContents);
+
+  return confjg;
+}
